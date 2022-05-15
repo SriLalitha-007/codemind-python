9d6bf73af687ccc1756d8e467cfe3787752d0a5b
SriLalitha-007 <96735263+SriLalitha-007@users.noreply.github.com>
2022-05-15 22:34:58 +0530

    def isPalindrome(n: int) -> bool:
        rev = 0
        i = n
        while i > 0:
            rev = rev * 10 + i % 10
            i //= 10
     
        
        return (n == rev)
     
    def countPal(minn: int, maxx: int) -> None:
        for i in range(minn, maxx + 1):
            if isPalindrome(i):
                print(i, end = " ")
     
    if __name__ == "__main__":
        countPal(int(input()),int(input()))

---
+++ Program_to_print_all_palindromes_in_a_given_range_.py
@@ -0,0 +1,21 @@
+def isPalindrome(n: int) -> bool:
+ 
+    
+    rev = 0
+    i = n
+    while i > 0:
+        rev = rev * 10 + i % 10
+        i //= 10
+ 
+    
+    return (n == rev)
+ 
+def countPal(minn: int, maxx: int) -> None:
+    for i in range(minn, maxx + 1):
+        if isPalindrome(i):
+            print(i, end = " ")
+ 
+if __name__ == "__main__":
+    countPal(int(input()),int(input()))
+    
+ 
