486e15a7d03fc3576a341b8a2d1d79799989df3b
SriLalitha-007 <96735263+SriLalitha-007@users.noreply.github.com>
2022-05-23 21:43:58 +0530

    num=int(input()) a=list(map(int,input().split())) odd=0 for i in range (num):     if a[i]%2!=0:         odd+=1 if odd<=2:     print("YES") else:     print("NO")

---
+++ Can_Robber_Escape?.py
@@ -0,0 +1,12 @@
+num=int(input())
+a=list(map(int,input().split()))
+odd=0
+for i in range (num):
+    if a[i]%2!=0:
+        odd+=1
+if odd<=2:
+    print("YES")
+else:
+    print("NO")
+    
+    
