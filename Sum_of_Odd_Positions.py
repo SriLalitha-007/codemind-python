867dc349af0df1677d83d64933aec8ce96c51cc3
SriLalitha-007 <96735263+SriLalitha-007@users.noreply.github.com>
2022-07-06 21:54:01 +0530

    a=int(input()) s=0 b=list(map(int,input().split())) for i in range(a):     if i%2==1:         s+=b[i] print(s)

--- Sum_of_Odd_Positions.py
+++ Sum_of_Odd_Positions.py
@@ -1,9 +1,7 @@
-num=int(input())
-a=list(map(int,input().split()))
+a=int(input())
 s=0
-length=len(a)
-for i in range(0,length):
-    if i%2!=0:
-        s+=a[i]
+b=list(map(int,input().split()))
+for i in range(a):
+    if i%2==1:
+        s+=b[i]
 print(s)
-    
